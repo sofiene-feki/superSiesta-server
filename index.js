57b29a7f97868f0f222cf5025e89c1c0fbb6d3fa
sofiene-feki <sofian.feki@gmail.com>
2025-08-19 12:04:16 +0200

    update

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ mongoose
   .catch((err) => console.error(err));
 
 // load routes dynamically
-readdirSync("./Routes").map((r) => {
-  const route = require(`./Routes/${r}`);
+readdirSync("./routes").map((r) => {
+  const route = require(`./routes/${r}`);
   if (route && typeof route === "function") {
     app.use("/api", route);
     console.log(`Route ${r} loaded successfully.`);
